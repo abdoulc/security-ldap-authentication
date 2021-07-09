a84966db0bd3da9519935c805de609e31c6e6fd6
abdoul razak <abdoulrazak1996@gmail.com>
2021-07-09 19:42:26 +0000

    Initial commit

---
+++ security-ldap-authentification/src/test/java/com/abdel/authldap/SecurityLdapAuthentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdel.authldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityLdapAuthentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
